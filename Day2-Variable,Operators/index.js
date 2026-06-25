bfc53ca0c93928eb3c76fdeec22fd1bace2da52d
shanmukatonangi <shanmukatonangi@gmail.com>
2026-06-25 21:32:35 +0530

    Variables and Operators

---
+++ Day2-Variable,Operators/index.js
@@ -0,0 +1,118 @@
+
+// let username="Shanmukh"
+// username="Praveen"
+
+// console.log(username)
+
+// const year="2026"
+
+// year="2027"
+
+// console.log(year)
+
+// before 2015
+
+// var sport="cricket"
+
+//string
+
+
+// "kgfsgfieriugeirugk"
+// 'kjfgksgfhsgK'
+
+// let fancynumber=999
+// fancynumber=9999
+// console.log(fancynumber)
+
+// let major=false
+
+
+//undefined
+// let city
+
+// console.log(city)
+
+// let user=null
+
+// console.log(user)
+
+
+
+// let id=Symbol("id")
+// console.log(id)
+
+//JS Operators
+
+//Arthemetic Operators -> + - * / % **
+
+// let a=10
+// let b=5
+// let add=a+b
+// let sub=a-b
+// let mul=a*b
+// let div=a/b
+// let rem=a%b
+// let expo=a**b  
+
+// console.log(add,sub,mul,div,rem,expo)
+
+//Assignment Operators
+
+// let x=10
+
+// x=x+10  // x+=10
+
+// x=x+2 //20+2=22// x+=2
+
+// x=x-5  //17
+
+// x*=2 //x=x*2  34
+
+// x/=2  //17
+
+// x%=2   //1
+
+// x**=100 ///1
+
+// console.log(x)
+
+
+// let x=10
+
+// x++
+
+//x++ //x=x+1 increment
+
+//x-- //x=x-1 decrement
+
+// console.log(x++)//first x gonna print 10 and then it gonna increase by 1 11
+// console.log(x)
+
+// console.log(--x)
+
+
+//Comparison Operators 
+
+// let a=10
+// let b=11
+//  let c =  a==b   //true
+// let d= a<b //true
+// let e= a>b //false
+// let f= a>=b  // true //false
+// let g= a<=b // true
+// let h= a !=b //true
+
+// console.log( c,d,e,f,g,h )
+
+//logical operators and or not
+
+// let logicaland= 10<=10 && 5>2
+// console.log(logicaland)
+
+// let logicalor= 10>20 || 10!=10
+// console.log(logicalor)
+
+let loggedinuser=true
+
+console.log(!loggedinuser)
+
