e5497921f7ce785b55a7e6d47e65426510466e62
shanmukatonangi <shanmukatonangi@gmail.com>
2026-07-11 22:11:40 +0530

    doubtsobjects

---
+++ Revision/topics.js
@@ -0,0 +1,126 @@
+// let numbers=[24,67,88,12,55]
+
+// let nums=[
+//     [10,20,30],
+//     [40,50,60],
+//     [70,80,90]
+// ]
+
+
+// console.log(nums[1])
+
+// //nums[1]=[40,50,60]
+// //nums[1][1]
+
+// // let nus=[1,2,3]
+// // nus.push(100)
+
+// nums[2].push(100)
+
+// 10
+// 20
+// 30
+// 40
+
+// let details={
+
+// }
+
+// let stu1name="Shanmukh"
+// let stu1roll=18
+// let stu2name="Praveen"
+// let stu1dept="cse"
+// let stu2roll=19
+// let stu2dept="cse"
+
+// let student1details={
+//     name:"Rahul",
+//     age:29,
+//     city:"Hyderabad",
+//     address:{
+//         state:"Telangana",
+//         district:"Hyderabad",
+//         blocks:[12,20,{appartmentname:"Classic Appartment",gates:[22,23,24]}]
+//     }
+// }
+
+// let stud1det=[{},{},{}]
+
+// console.log(student1details.name)//Rahul
+
+// student1details.address.blocks[2]={appartmentname:"Classic Appartment",gates:[22,23,24]}
+
+// /student1details.address.blocks[2]= tillblocks
+//tillblocks={appartmentname:"Classic Appartment",gates:[22,23,24]}
+// student1details.address.blocks[2].appartment="Old Appartments"
+//tillblock.gates[1]= 23
+//student1details.address.blocks[2].gates[1]= 23
+
+// let pro1={
+//     "id": 1,
+//     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//     "price": 109.95,
+//     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//     "rating": {
+//       "rate": 3.9,
+//       "count": 120
+//     }
+//   }
+
+
+
+//   console.log(pro1.image)
+
+// let studentroutine={
+//     name:"Shanmukh",
+//     dept:"CSE",
+//     studing:function(){
+//         console.log("Shanmukh is studying")
+//     }
+// }
+
+// studing()
+
+//studentroutine.dept
+// studentroutine.studing() //Methods
+// console.log() // js Method
+
+// let cnsole={
+//     random:"some random",
+//     log:function(){
+//         console.log("imagination of real console.log")
+
+//     }
+// }
+
+// cnsole.log()
+// nums.push()
+
+// let details={
+//     name:"Shanmukh"
+// }
+
+// let detailsconvertedtojson=JSON.stringify(details)//normal obj to json
+
+// console.log(detailsconvertedtojson)
+
+
+
+// let pro1=`{
+//     "id": 1,
+//     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//     "price": 109.95,
+//     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//     "rating": {
+//       "rate": 3.9,
+//       "count": 120
+//     }
+//   }`
+
+// let pro1tonormalobj=JSON.parse(pro1)
+
+// console.log(pro1tonormalobj)
