b3703e6429b1020ca786f1a8427580ef44df1ac4
shanmukatonangi <shanmukatonangi@gmail.com>
2026-07-17 11:21:54 +0530

    day 9 oop adv

---
+++ Day9-ADV-OOP/index.js
@@ -0,0 +1,115 @@
+// let student={
+//     name:"Shanmukh",
+//     city:"hyd",
+//     Working:function(){
+//         console.log(`${this.name} is working`)
+//     }
+// }
+
+// let studentwork=student.Working
+
+// studentwork()
+
+// let studentwork=function(){
+//         console.log(`${this.name} is working`)
+//     }
+
+
+// function studentwork(){
+//     console.log(`${this.name} is working`)
+// }
+
+
+// function Buynow(a,b,c){
+//         console.log(`${a} ${b} has purchased ${this.name}`)
+//     }
+
+// let pro1={
+//     name:"iphone 14",
+//     price:20000,
+   
+// }
+// //call
+// let bnc=Buynow.call(pro1,"shanmukh","praveen","sai")
+
+
+
+// let pro2={
+//     name:"iphone 15",
+//     price:20000,
+//      Buynow:function(){
+//         console.log(`${this.name} prchased`)
+//     }
+    
+   
+// }
+// Buynow.apply(pro2,["shanmukh","praveen","sai"])
+
+
+
+// let pro3={
+//     name:"iphone 16",
+//     price:20000,
+   
+    
+// }
+
+// let bnb=Buynow.bind(pro3)
+// bnb()
+
+
+// function pro3purchase(){
+//      console.log(`${a} ${b} has purchased ${pro3.name}`)
+
+
+// }
+// pro3purchase()
+
+
+// function attendance(){
+//     console.log(`${this.name} is present`)
+// }
+
+// let stu1={
+//     name:"SHanmukh"
+// }
+
+// attendance.call(stu1)
+// attendance.apply(stu1)
+
+// let stu1attendance=attendance.bind(stu1)
+// stu1attendance()
+
+
+let arr1=[1,2,3,4]
+// arr1.push(5)
+
+let arr2=[12,56,34,79]
+// arr2.push(66)
+
+
+console.dir(arr1)
+
+
+// class arr{
+//     constructor(){
+
+//     }
+
+//     push(){
+//         //whole logical ;art to actually perform push
+
+//     }
+//     pop(){
+
+//     }
+
+// }
+
+// let str="shanmukh"
+
+// console.log(str.toUpperCase())
+
+
+// something.something()
+
